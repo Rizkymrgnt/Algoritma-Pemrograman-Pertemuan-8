06b5c0983eff2c5bddbab88cb00ec908ed4b5603
Rizkymrgnt <146719909+Rizkymrgnt@users.noreply.github.com>
2025-04-23 19:13:46 +0700

    Add files via upload

---
+++ masukanKata.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukan Kata = HALLO"<<endl;
+	cin>>kata;
+	
+	if (kata=="HALLO"){
+		cout<<"Kata yang dimasukan sesuai"<<endl;
+	}else{
+		cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	
+	}
+}
